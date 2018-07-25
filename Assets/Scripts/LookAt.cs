cc663993faa8d57ca5548a721dc4480f1c701e60
Jing Tan <jtan1993@gmail.com>
2018-07-25 21:22:54 +0100

    LookAt

---
+++ Assets/Scripts/LookAt.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//use LookAt to make an object's forward direction point to another transform in the world
+
+public class LookAt : MonoBehaviour {
+
+    public Transform lookAtTarget; 
+	
+	void Update () {
+
+        //apply to camera and assign an object to track
+
+        transform.LookAt(lookAtTarget); 
+	}
+}
