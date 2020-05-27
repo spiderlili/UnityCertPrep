7d67e68da8f55661d2a9858673ac3e83e002baec
Jing <jtan1993@gmail.com>
2020-05-27 14:13:22 +0100

    added statics

---
+++ Assets/Scripts/Statics/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private UIManager _uiManager;
+    private void OnEnable()
+    {
+        SpawnManager.enemyCount++;
+        _uiManager = GameObject.Find("UI Manager").GetComponent<UIManager>();
+        _uiManager.UpdateEnemyCount();
+        Die();
+    }
+
+    private void OnDisable()
+    {
+        SpawnManager.enemyCount--;
+        _uiManager.UpdateEnemyCount();
+    }
+
+    void Die()
+    {
+        Destroy(this.gameObject, Random.Range(2, 6));
+    }
+}
