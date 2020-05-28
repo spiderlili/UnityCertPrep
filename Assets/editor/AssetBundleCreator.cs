dfa15ef08ab2552ef2c4057fe804fbb5d113c9f1
Jing <jtan1993@gmail.com>
2020-05-28 21:29:39 +0100

    built asset bundles

---
+++ Assets/editor/AssetBundleCreator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AssetBundleCreator : Editor
+{
+    [MenuItem("Assets/Build Asset Bundles")] //appear under Assets menu
+    static void BuilAll() //get run when selected from menu
+    {
+        //path To Folder: where the asset bundle will go, how it will be built out(chunk based compression), target platform
+        //to be compatible with all platforms: need to create an asset bundle for each platform and load them based on platform
+        BuildPipeline.BuildAssetBundles("Assets/Scripts/AssetBundles/AssetBundlesTest", BuildAssetBundleOptions.ChunkBasedCompression,
+            BuildTarget.StandaloneWindows);
+    }
+}
