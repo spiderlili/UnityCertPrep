72bcd9c1898f9d1ae1b9b77b88e46b5245abbab1
Jing <jtan1993@gmail.com>
2020-05-28 19:40:56 +0100

    added rpg item database

---
+++ Assets/Scripts/Classes/ItemDatabase.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDatabase : MonoBehaviour
+{
+    [SerializeField]
+    RPGItem sword;
+    RPGItem hammer;
+    RPGItem bread;
+
+    //populate the database via inspector
+    public RPGItem[] items;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //doesn't make sense to init items this way if have many items (>10): constructor is easier to manage
+        sword = new RPGItem();
+        sword.name = "Sword";
+        sword.id = 1;
+        sword.description = "Legendary Sword";
+
+        //constructor method:
+        hammer = new RPGItem("Hammer", 2, "Giant Hammer");
+
+        //create items via another custom function
+        bread = CreateItem("Bread", 3, "Tasty");
+    }
+
+    //a function that creates items:
+    private RPGItem CreateItem(string name, int id, string description)
+    {
+        //create a generic item
+        var item = new RPGItem(name, id, description);
+        return item; //must return to retrieve the item created
+    }
+}
---
+++ Assets/Scripts/Classes/RPGItem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable] //makes class visible in the inspector
+public class RPGItem 
+{
+    public string name;
+    public int id;
+    public string description;
+
+    public RPGItem() //default empty constructor with no parameters
+    {
+
+    }
+
+    public RPGItem(string name, int id, string description)
+    {
+        this.name = name;
+        this.id = id;
+        this.description = description;
+    }
+}
