84d81fd6f03c1a30ad8b0d0113deb80b96b21adf
Jing <jtan1993@gmail.com>
2020-05-29 15:46:16 +0100

    added coroutine example

--- Assets/Scripts/Coroutines/SlideshowCoroutine.cs
+++ Assets/Scripts/Coroutines/SlideshowCoroutine.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SlideshowCoroutine : MonoBehaviour
 {
+    public RawImage display;
+    public Texture[] slides;
+    public float displayTime = 5;
+    private RectTransform displayRectTransform;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        displayRectTransform = display.GetComponent<RectTransform>();
+        StartCoroutine(Display());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Display()
     {
-        
+        while (true) //loop code indefinitely
+        {
+            foreach(Texture slide in slides)
+            {
+                displayRectTransform.sizeDelta = new Vector2(slide.width, slide.height);
+                display.texture = slide;
+                yield return new WaitForSeconds(displayTime);
+            }
+        }
     }
 }
