5bd53bca4153354c553921c586907ebc81a41a6e
Jing <jtan1993@gmail.com>
2020-05-29 14:45:10 +0100

    added customer database

---
+++ Assets/Scripts/Classes/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Cache;
+using UnityEngine;
+
+[System.Serializable]
+public class Customer 
+{
+    public string firstname, lastName;
+    public int age;
+    public enum customerGender { Male = 0, Female = 1, Other = 2};
+    public customerGender gender;
+    public string occupation;
+
+    public Customer(string firstName, string lastName, int age, int gender, string occupation)
+    {
+        
+        this.firstname = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.gender = (customerGender)gender; //cast int to enum
+        this.occupation = occupation;
+    }
+}
+
---
+++ Assets/Scripts/Classes/CustomerDatabase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerDatabase : MonoBehaviour
+{
+    public Customer[] customers;
+    [SerializeField] Customer customer1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        customer1 = new Customer("Kat", "Wong", 22, 1, "Student");
+        Customer customer2 = new Customer("Casey", "Tan", 32, 1, "Teacher");
+        Customer customer3 = new Customer("Jay", "Smith", 22, 1, "Marketer");
+    }
+}
