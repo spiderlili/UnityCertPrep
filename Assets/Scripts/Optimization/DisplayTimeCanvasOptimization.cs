7b142b84db501e73cae36fcf46150049a7ba6cb0
Jing <jtan1993@gmail.com>
2020-05-30 16:09:42 +0100

    added nested canvas example

---
+++ Assets/Scripts/Optimization/DisplayTimeCanvasOptimization.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using TMPro;
+using UnityEngine.UI;
+
+//display current time as text
+public class DisplayTimeCanvasOptimization : MonoBehaviour
+{
+    private TextMeshProUGUI timeDisplay;
+    DateTime currentTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeDisplay = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime = DateTime.Now;
+        timeDisplay.text = currentTime.ToString("T"); //formats the time as HH:MM:SS:XM - 4:00:00 PM
+    }
+}
