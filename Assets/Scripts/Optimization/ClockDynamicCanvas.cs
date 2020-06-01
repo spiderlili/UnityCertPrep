b0018183527e036540b43be9c19afe7c835ed0b6
Jing <jtan1993@gmail.com>
2020-06-01 12:00:30 +0100

    added static vs dynamic canvas example

---
+++ Assets/Scripts/Optimization/ClockDynamicCanvas.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class ClockDynamicCanvas : MonoBehaviour
+{
+    private TextMeshProUGUI clock;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        clock = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        clock.text = DateTime.Now.ToString();
+    }
+}
---
+++ Assets/Scripts/Optimization/ImageMover.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+//move the image around in a circular spiral motion, centred around baseRectTransform
+public class ImageMover : MonoBehaviour
+{
+    public RectTransform baseRectTransform; //anchor image to the centre of the baseRectTransform (canvas)
+    private float minDistance, maxDistance, distance;
+    private float spinSpeed = 4;
+    private RectTransform rectTransform;
+    private Vector3 basePosition;
+    private Vector3 positionOffset = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        basePosition = baseRectTransform.position;
+        minDistance = Screen.height * 0.1f;
+        maxDistance = Screen.height * 0.3f;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Mathf.Lerp(minDistance, maxDistance, Mathf.Cos(Time.time) * 0.5f + 0.5f);
+        positionOffset.x = distance * Mathf.Cos(Time.time * spinSpeed);
+        positionOffset.y = distance * Mathf.Sin(Time.time * spinSpeed);
+        rectTransform.position = basePosition + positionOffset;
+    }
+}
