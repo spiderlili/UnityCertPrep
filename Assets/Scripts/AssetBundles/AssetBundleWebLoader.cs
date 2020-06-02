194fcf78e89cc9c836506c131c5f84268a22f9a3
Jing <jtan1993@gmail.com>
2020-06-02 13:53:25 +0100

    added assetbundle web loader

---
+++ Assets/Scripts/AssetBundles/AssetBundleWebLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEditor.VersionControl;
+using UnityEngine.Networking;
+
+public class AssetBundleWebLoader : MonoBehaviour
+{
+    public string bundleUrl = "http://localhost/assetbundles/testbundle";
+    public string assetName = "BundledObject";
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        using (UnityWebRequest web = new UnityWebRequest(bundleUrl))
+        {
+            yield return web;
+            UnityWebRequest remoteAssetBundle = UnityWebRequestAssetBundle.GetAssetBundle(bundleUrl);
+            if(remoteAssetBundle == null)
+            {
+                Debug.LogError("Failed to download AssetBundle");
+                yield break;
+            }
+            AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(remoteAssetBundle);
+            Instantiate(bundle.LoadAsset(assetName));
+            bundle.Unload(false);
+        }
+    }
+}
---
+++ Assets/Scripts/AssetBundles/LoadAssetBundleFromFileAsync.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+//Fastest way to load assetbundle: runs as a coroutine to allow a project to continue to run while asset bundle is loaded
+public class LoadAssetBundleFromFileAsync : MonoBehaviour
+{
+    public string bundleName;
+    public string assetName;
+
+    IEnumerator Start()
+    {
+        AssetBundleCreateRequest asyncBundleRequest = AssetBundle.LoadFromFileAsync(Path.Combine(Application.streamingAssetsPath, bundleName));
+        yield return asyncBundleRequest;
+
+        AssetBundle localAssetBundle = asyncBundleRequest.assetBundle;
+
+        if(localAssetBundle == null)
+        {
+            Debug.LogError("Failed to load AssetBundle");
+            yield break;
+        }
+
+        AssetBundleRequest assetRequest = localAssetBundle.LoadAssetAsync<GameObject>(assetName);
+        yield return assetRequest;
+
+        GameObject prefab = assetRequest.asset as GameObject;
+        Instantiate(prefab);
+
+        localAssetBundle.Unload(false);
+    }
+}
