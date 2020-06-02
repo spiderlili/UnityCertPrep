61029621cd132fa7f0c1244c8b7b73a0fee12e4f
Jing <jtan1993@gmail.com>
2020-06-02 12:02:04 +0100

    modified asset bundle creator

--- Assets/editor/AssetBundleCreator.cs
+++ Assets/editor/AssetBundleCreator.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System.IO;
 
 public class AssetBundleCreator : Editor
 {
     [MenuItem("Assets/Build Asset Bundles")] //appear under Assets menu
-    static void BuilAll() //get run when selected from menu
+    static void BuilAllAssetBundles() //get run when selected from menu
     {
+        string assetBundleDirectory = "Assets/StreamingAssets";
+
+        if (!Directory.Exists(Application.streamingAssetsPath))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
         //path To Folder: where the asset bundle will go, how it will be built out(chunk based compression), target platform
         //to be compatible with all platforms: need to create an asset bundle for each platform and load them based on platform
-        BuildPipeline.BuildAssetBundles("Assets/Scripts/AssetBundles/AssetBundlesTest", BuildAssetBundleOptions.ChunkBasedCompression,
-            BuildTarget.StandaloneWindows);
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.ChunkBasedCompression,
+            BuildTarget.StandaloneWindows); //or: EditorUserBuildSettings.activeBuildTarget for auto target detection
     }
 }
