e3eac9d2f5c87023d9e3a467f5847b4dc8f71eda
Jing <jtan1993@gmail.com>
2020-06-06 11:41:54 +0100

    added event example

---
+++ Assets/Scripts/DelegateEvents/CubeEvent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//each cube will share this script and register the TurnRed() method to onClick event
+public class CubeEvent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //subscribe to onClick event, register a method that matches the delegate signature (void method)
+        EventMain.onClick += TurnRed;
+    }
+
+    public void TurnRed()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
+    //when subscribing: always need to unsubscribe when destroying the object to avoid creating errors
+    private void OnDisable()
+    {
+        EventMain.onClick -= TurnRed;
+    }
+}
---
+++ Assets/Scripts/DelegateEvents/EventMain.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventMain : MonoBehaviour
+{
+    public delegate void ActionClick(); //delegate to store methods for oncomplete event
+    public static event ActionClick onClick; //event to allow objects to subscribe to button click
+
+    public void ButtonClick()
+    {
+        if (onClick != null) //null check on events to prevent error - onClick must have at least 1 listener
+        {
+            onClick();
+        }
+    }
+}
