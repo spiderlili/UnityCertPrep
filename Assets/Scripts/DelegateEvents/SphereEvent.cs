d77448e1fe767b41e6ee4127e82f682ab0ef8d0a
Jing <jtan1993@gmail.com>
2020-06-06 12:40:16 +0100

    changed events scene

--- Assets/Scripts/DelegateEvents/SphereEvent.cs
+++ Assets/Scripts/DelegateEvents/SphereEvent.cs
@@ -7,13 +7,7 @@ public class SphereEvent : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        EventMain.onClick += Fall;
     }
 
     public void Fall()
