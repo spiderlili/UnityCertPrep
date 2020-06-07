afd8525484ef64c59ce4d642fc5c386e8d99fc53
Jing <jtan1993@gmail.com>
2020-06-07 14:57:36 +0100

    added teleport example

---
+++ Assets/Scripts/DelegateEvents/ObjectTeleport.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectTeleport : MonoBehaviour
+{
+    private void Start()
+    {
+        TeleportEventMain.onTeleport += Spawn; //register for event
+    }
+
+    private void OnDisable()
+    {
+        TeleportEventMain.onTeleport -= Spawn; //de-register if object is destroyed
+    }
+
+    //invoke the method from the event, pass in parameter assigned
+    public void Spawn(Vector3 pos) //when the player dies, they have their respawn position - must match onTeleport delegate method signature
+    {
+        transform.position = pos;
+    }
+}
---
+++ Assets/Scripts/DelegateEvents/TeleportEventMain.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//object should subscribe to the event and be invoked
+public class TeleportEventMain : MonoBehaviour
+{
+    public delegate void ActionTeleport(Vector3 pos); //delegate signature is a method to accept a position
+    public static event ActionTeleport onTeleport; //static so it runs along the entire class - don't need new instances
+
+    public void TeleportSpacePress()
+    {
+        if(onTeleport != null)
+        {
+            Vector3 pos = new Vector3(5, 2, 0);
+            onTeleport(pos);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TeleportSpacePress(); //invoke onTeleport method
+        }
+    }
+}
