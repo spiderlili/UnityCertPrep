99cd0a195d68fd3e200979b2898a6c1a3e5406b5
Jing <jtan1993@gmail.com>
2020-06-08 11:31:13 +0100

    added actions example

---
+++ Assets/Scripts/DelegateEvents/PlayerActions.cs
@@ -0,0 +1,35 @@
+﻿using System; //allow access to code libraries for using actions
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerActions : MonoBehaviour
+{
+    //traditional delegate and event system:
+    //public delegate void OnDamageReceived(int currentHealth); //other managers can subscribe to damage function 
+    //best practice to make events static so you don't have to instantiate an instance of this PlayerActions class - allow it to run along all classes
+    //public static event OnDamageReceived onDamage; //people subscribed to onDamage event will look for OnDamageReceived() signature
+
+    //action system: can clean both delegate and event up into 1 line of code
+    public static Action<int> onDamageReceived;
+
+    public int Health { get; set; }
+    private void Start()
+    {
+        Health = 10;
+    }
+
+    void Damage()
+    {
+        Health--;
+        
+        //check if anyone wants to be notified
+        if(onDamageReceived != null)
+        {
+            onDamageReceived(Health); //Raise the onDamage event
+        }
+
+        //problem with traditional approach: player has to know about the ui manager
+        //UIManager.UpdateHealth();
+    }
+}
---
+++ Assets/Scripts/DelegateEvents/UIManagerActions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManagerActions : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        //subscribe to player onDamage event
+        PlayerActions.onDamageReceived += UpdateHealth;
+        
+    }
+
+    private void OnDisable()
+    {
+        //unsubscribe to player onDamage event
+        PlayerActions.onDamageReceived -= UpdateHealth;
+    }
+
+    public void UpdateHealth(int health) //method has to match the delegate signature
+    {
+        //display updated health
+        Debug.Log("health: " + health);
+    }
+}
