6c34f3f5c4a21592b0bf91fe2fd9d9d564d8fe5a
Jing <jtan1993@gmail.com>
2020-06-09 18:05:06 +0100

    added return type delegate func example

---
+++ Assets/Scripts/DelegateEvents/StringLengthFunctionDelegate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class StringLengthFunctionDelegate : MonoBehaviour
+{
+    //takes in a string and returns its length
+    //public delegate int characterStringLength(string characterText);
+    public Func<string, int> strLength;
+
+    void Start()
+    {
+        //using a return type delegate: just like actions but with func
+        strLength = GetCharacters; //subscribe method to it - signature string as a parameter and return type int match
+        int lengthOfString = strLength("Jon"); 
+        Debug.Log("Character Count: " + lengthOfString);
+
+        //traditional delegate method: create delegate instance, assign delegate charStrLength to that method
+        //characterStringLength charStrLength = new characterStringLength(GetCharacters);
+        //Debug.Log(charStrLength("Jon"));
+
+        //traditional non-delegate method using functions
+        //int characterCount = GetCharacters(name);
+        //Debug.Log("Character Count: " + characterCount);
+    }
+
+    int GetCharacters(string name) //must match delegate method signature
+    {
+        return name.Length;
+    }
+}
