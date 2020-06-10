a4a14269b3a58a9a49ed759978ce0f1c501076e3
Jing <jtan1993@gmail.com>
2020-06-10 13:59:12 +0100

    added slerp example

---
+++ Assets/AimTargetSlerp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimTargetSlerp : MonoBehaviour
+{
+    [SerializeField] private Transform targetToAim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(targetToAim != null)
+        {
+            Vector3 directionToFace = targetToAim.position - this.transform.position;
+            Debug.DrawRay(this.transform.position, directionToFace, Color.green);
+            Quaternion targetRotation = Quaternion.LookRotation(directionToFace);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 5.0f); //5 meters per second for rotation
+        }
+    }
+}
