d8ffcdb3710716d85ebe8c18b707bbcb70381fbc
Jing <jtan1993@gmail.com>
2020-06-11 15:04:44 +0100

    added array loop

---
+++ Assets/Scripts/DataStructures/Arrays/ArrayLoopPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayLoopPlayer : MonoBehaviour
+{
+    public int[] itemID;
+    public string[] itemName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            for(int i = 0; i < itemName.Length; i++)
+            {
+                if(itemName[i] == "Dagger")
+                {
+                    Debug.Log("Dagger exsits");
+                    Debug.Log("Dagger item ID: " + itemID[i]);
+                }
+                //Debug.Log(itemName[i]);
+            }
+        }    
+    }
+}
