00d06a112544a72c8bd0f81f34632f741f963878
Jing <jtan1993@gmail.com>
2020-06-12 13:52:12 +0100

    added name list manager

--- Assets/Scripts/DataStructures/Lists/NameListManager.cs
+++ Assets/Scripts/DataStructures/Lists/NameListManager.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class NameListManager : MonoBehaviour
 {
+    [SerializeField] private List<string> names = new List<string>();
     // Start is called before the first frame update
     void Start()
     {
-        
+        names.Add("Kate");
+        names.Add("Anna");
+        names.Add("Cory");
+        names.Add("Beatrice");
+        names.Add("Faye");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var nameToRemove = names[(Random.Range(0, names.Count))]; //temp variable
+            names.Remove(nameToRemove);
+
+            foreach (var name in names)
+            {
+                Debug.Log(name);
+            }
+
+            Debug.Log("name removed: " + nameToRemove);
+        }
     }
 }
