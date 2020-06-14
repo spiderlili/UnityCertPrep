43cfe79250b6d6d2a188486a5eca805ff4b26f66
Jing <jtan1993@gmail.com>
2020-06-14 11:22:15 +0100

    added simple player movement example

---
+++ Assets/Scripts/Utilities/PlayerMovementInputSimple.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementInputSimple : MonoBehaviour
+{
+    [SerializeField] private float _horizontalSpeed = 1.0f;
+    [SerializeField] private float _verticalSpeed = 1.0f;
+    [SerializeField] private bool _disableVerticalInput = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame - about 60 frames per second
+    void Update()
+    {
+        //auto move cube to the right extremely quick: moving frames per second, 1m per frame => 60m per second
+        //transform.Translate(Vector3.right); //new Vector3(1,0,0)
+
+        //move to the right 1m per second: convert frame rate to seconds using Time.deltaTime and smooth out between frames
+        //transform.Translate(Vector3.right * Time.deltaTime * _speed); 
+
+        //use input auto-mapped to horizontal & vertical axis - assign to input manager
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector3(horizontalInput, 0, 0) * _horizontalSpeed * Time.deltaTime);
+
+        if(_disableVerticalInput == true)
+        {
+            float verticalInput = Input.GetAxis("Vertical");
+            transform.Translate(new Vector3(0, verticalInput, 0) * _verticalSpeed * Time.deltaTime);
+        }
+    }
+}
