f6434f1b82b85e4d8519f7524c634962e0627745
Jing <jtan1993@gmail.com>
2020-06-14 11:33:43 +0100

    added collectable

---
+++ Assets/Scripts/Utilities/CollectableObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableObject : MonoBehaviour
+{
+    //get info about object that collided with you, collider other stores info about whatever object hit like player
+    private void OnTriggerEnter(Collider other) 
+    {
+        //check who hit me: if only want player to collect me
+        if(other.tag == "Player")
+        {
+            //TODO: add VFX, points / powerup ability
+            Destroy(this.gameObject);
+        }
+    }
+}
