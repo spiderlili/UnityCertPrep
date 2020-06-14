2e549756638348175a79f1cd3d6eb76cb130346b
Jing <jtan1993@gmail.com>
2020-06-14 11:44:08 +0100

    added pause example

---
+++ Assets/Scripts/Utilities/PauseTimeScale.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.UI;
+using UnityEngine.UI;
+
+public class PauseTimeScale : MonoBehaviour
+{
+    [SerializeField] private Image pauseOverlay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseOverlay.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Time.timeScale = 0;
+            pauseOverlay.enabled = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Time.timeScale = 1;
+            pauseOverlay.enabled = false;
+        }
+    }
+}
