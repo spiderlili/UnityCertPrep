6d04a6a513356bbda9f732c1c5ab6da89cf6b103
Jing <jtan1993@gmail.com>
2020-06-18 08:06:23 +0100

    added name linq

--- Assets/Scripts/LINQ/LINQNameFinderAny.cs
+++ Assets/Scripts/LINQ/LINQNameFinderAny.cs
@@ -7,15 +7,21 @@ using System.Linq;
 //TODO: make it return true regardless of capital case
 public class LINQNameFinderAny : MonoBehaviour
 {
-    public string[] names = { "jon", "katy", "alex", "jessie" };
+    public string[] names = { "jon", "katy", "alex", "jessie", "katy", "james", "james", "matilda" };
     public string searchForName = "jon";
+    public bool printAllUniqueNames = false;    
+    public bool printAllLongNames = false;
+    public int longNameCharactersThreshold = 5;
+
     // Start is called before the first frame update
     void Start()
-    {
-        var nameFound = names.Any(name => name == searchForName); //returns t/f if searchString is within the array
+    {        
+        //search if a name exists
+        var nameFound = names.Contains(searchForName);
+        //var nameFound = names.Any(name => name == searchForName); //returns t/f if searchString is within the array
         Debug.Log("name found: " + nameFound);
 
-        //traditional solution using a loop
+        //traditional solution using a loop: search if a name exists
         /*
         foreach(var name in names)
         {
@@ -24,5 +30,25 @@ public class LINQNameFinderAny : MonoBehaviour
                 Debug.Log("Found Jon");
             }
         }*/
+
+        //search for distinct names: take the names, return the orginals and put into a new collection
+        if(printAllUniqueNames == true)
+        {
+            var uniqueNames = names.Distinct();
+            foreach (var uniqueName in uniqueNames)
+            {
+                Debug.Log("Unique Name: " + uniqueName);
+            }
+        }
+
+        //store long names > 5 characters 
+        if(printAllLongNames == true)
+        {
+            var longNameResults = names.Where(name => name.Length > longNameCharactersThreshold);
+            foreach (var longName in longNameResults)
+            {
+                Debug.Log("Long Names: " + longName);
+            }
+        }
     }
 }
