696e14c247ae34a339bde6b81f1dacb9d7817659
Jing <jtan1993@gmail.com>
2020-06-19 23:07:58 +0100

    added average linq example

--- Assets/Scripts/LINQ/LINQGradeFilter.cs
+++ Assets/Scripts/LINQ/LINQGradeFilter.cs
@@ -6,16 +6,22 @@ using System;
 
 //The quiz grades should be between 0 – 100. 
 //Filter through the quiz grades, create a new collection of only passing grades > 80 
+//order the grades in a descending order
 public class LINQGradeFilter : MonoBehaviour
 {
     private int[] grades = {90, 80, 70, 60, 65};
     public int passThreshold = 80;
     private List<int> passedGrades = new List<int>();
+    public bool orderByDescending = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        var passGrades = grades.Where(grade => grade >= passThreshold);
+        var passGrades = grades.Where(grade => grade >= passThreshold).OrderByDescending(g => g);
+
+        //order by reverse
+        //var passGrades = grades.Where(grade => grade >= passThreshold).OrderByDescending(g => g).Reverse();
+
         foreach(var grade in passGrades)
         {
             Debug.Log("Passing Grades" + grade);
---
+++ Assets/Scripts/LINQ/LinqItemClassFilter.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UIElements;
+using System;
+
+namespace LINQ {
+    [System.Serializable]
+    public class Item
+    {
+        public string Name;
+        public int ID;
+        public int buff;
+    }
+
+    public class LinqItemClassFilter : MonoBehaviour
+    {
+        public List<Item> items;
+
+        //check if itemID3 exists in the list, print out t/f
+        //grab all items with buff > 100 
+        //calculate average of all buff stats
+        private void Start()
+        {
+            bool isItemID3Here = items.Any(item => item.ID == 3);
+            Debug.Log("itemID3 exists: " + isItemID3Here);
+
+            var itemBigBuff = items.Where(item => item.buff > 100);
+
+            foreach(var bigBuff in itemBigBuff)
+            {
+                Debug.Log("item with buff > 100: " + bigBuff.Name);
+            }
+
+            var averageBuff = items.Average(item => item.buff);
+            Debug.Log("average buff: " + averageBuff);
+
+            /* traditional average solution
+            int buffSum = 0;
+            foreach (var item in items)
+            {
+                buffSum += item.buff;               
+            } 
+
+            int averageBuff = buffSum / items.Count;
+            Debug.Log("average buff: " + averageBuff);
+            */
+        }
+    }
+}
