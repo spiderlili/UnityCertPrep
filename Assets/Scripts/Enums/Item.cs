17889577d2c4b291ac571da5c5206530081f8b13
Jing <jtan1993@gmail.com>
2020-06-20 23:59:42 +0100

    added item detector

---
+++ Assets/Scripts/Enums/Item.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enum 
+{ 
+[System.Serializable]
+public class Item
+    {
+        public string itemName;
+        public int ID;
+        public Sprite icon;
+
+        public enum ItemType
+        {
+            None,
+            Weapon,
+            Consumable
+        }
+
+        public ItemType itemType;
+
+        public void DetectTypeAction()
+        {
+            switch (itemType)
+            {
+                case ItemType.Weapon:
+                    Debug.Log("it's a weapon");
+                    break;
+                case ItemType.Consumable:
+                    Debug.Log("it's a consumable");
+                    break;
+                case ItemType.None:
+                    Debug.Log("it's none");
+                    break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Enums/ItemDatabase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Enum;
+
+namespace Enum 
+{ 
+    public class ItemDatabase : MonoBehaviour
+    {
+        public List<Item> itemDatabase = new List<Item>();
+
+        private void Start()
+        {
+            itemDatabase[0].DetectTypeAction();
+        }
+    }
+}
