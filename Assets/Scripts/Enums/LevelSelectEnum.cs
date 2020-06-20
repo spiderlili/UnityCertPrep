0955b0b97b5e0eb69e1dfefe03e6260c735e2b7f
Jing <jtan1993@gmail.com>
2020-06-20 23:48:59 +0100

    added level select enum

---
+++ Assets/Scripts/Enums/LevelSelectEnum.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectEnum : MonoBehaviour
+{
+    public enum Difficulty
+    {
+        Easy, //0
+        Normal, //1
+        Hard, //2
+        Expert //3
+    }
+    public Difficulty selectedDifficulty;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadScene((int)selectedDifficulty); //cast enum to an int
+    }
+}
