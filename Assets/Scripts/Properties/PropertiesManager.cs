f22bf9a0b55a61da775083a105d5da946758bd25
Jing <jtan1993@gmail.com>
2020-06-21 20:26:08 +0100

    added property

---
+++ Assets/Scripts/Properties/PropertiesManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestProperty
+{
+    PropertiesManager mg = new PropertiesManager();
+
+    public void Test()
+    {
+        //mg.Speed = 5f; //error due to read only property
+    }
+}
+
+public class PropertiesManager : MonoBehaviour
+{
+    public string Name { get; set; } 
+    private float _speed; //read only
+
+    public float SpeedAutoProperty { get; private set; } //auto property requires both the getter and setter
+
+    public float Speed
+    {
+        get
+        {
+            return _speed; //access private data without changing it
+        }
+
+        private set
+        {
+            _speed = value;
+        }
+    }
+
+    private void Start()
+    {
+        Speed = 10f;
+        Debug.Log(Speed);
+    }
+}
