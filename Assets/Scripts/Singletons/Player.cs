3f8a822567c780e6ab42457f1469922034862526
Jing <jtan1993@gmail.com>
2020-06-25 22:44:32 +0100

    added ui manager example

---
+++ Assets/Scripts/Singletons/Player.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Singleton { 
+    public class Player : MonoBehaviour
+    {
+    // Start is called before the first frame update
+        void Start()
+        {
+            UIManager.UIManagerInstance.UpdateScore(40);
+        }
+    }
+}
---
+++ Assets/Scripts/Singletons/UIManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Singleton{
+    public class UIManager : MonoBehaviour
+    {
+        //singleton requires a static private var to declare the instance of this class
+        private static UIManager _uiManagerInstance;
+        //public property to access _uiManagerInstance: never create a set to allow changes
+        public static UIManager UIManagerInstance 
+        {
+            get
+            {
+                if (_uiManagerInstance == null)
+                {
+                    Debug.LogError("UI Manager is null");
+                }
+                return _uiManagerInstance;
+            }
+        }
+
+        private void Awake()
+        {
+            _uiManagerInstance = this;
+        }
+
+        public void UpdateScore(int score)
+        {
+            Debug.Log("Score: " + score);
+            Debug.Log("Notifying the game manager");
+            //Manager classes can talk to each other BUT they can never talk to an object that's not a singleton. 
+            GameManager.Instance.DisplayName();
+        }
+    }
+}
