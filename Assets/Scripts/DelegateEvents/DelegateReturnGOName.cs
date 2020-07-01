320639a4bafae7cbdcf391026a97e02b54eb91ce
Jing <jtan1993@gmail.com>
2020-07-01 08:00:48 +0100

    added delegate print name

---
+++ Assets/Scripts/DelegateEvents/DelegateReturnGOName.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelegateReturnGOName : MonoBehaviour
+{
+    //create a delegate of type void (action) that has no parameters => return the game object's name
+    public Action onGetName;
+
+    private void Start()
+    {
+        //use a lambda to eliminate the need of a method: () => no parameters go to
+        //onGetName = () => Debug.Log("Name: " + gameObject.name);
+
+        //to add more game logic
+        onGetName = () =>
+        {
+            Debug.Log("Name: " + gameObject.name);
+            Debug.Log("More Game Logic");
+        };
+
+        //call the action delegate like a method
+        onGetName(); 
+    }
+
+    //legacy way without using delegates
+    /*private void GetGOName()
+    {
+        Debug.Log("Name: " + gameObject.name); //return name of the game object this script is on
+    }*/
+}
