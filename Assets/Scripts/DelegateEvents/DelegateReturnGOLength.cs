7c12599d6ef35cd00c34afa1f13ea96b650c6725
Jing <jtan1993@gmail.com>
2020-07-02 22:15:50 +0100

    added obj length

---
+++ Assets/Scripts/DelegateEvents/DelegateReturnGOLength.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DelegateReturnGOLength : MonoBehaviour
+{
+    //using type return: Func. if using void: Action
+    public Func<int> onGetName;
+    private void Start()
+    {
+        onGetName = () => this.gameObject.name.Length;
+        int characterCount = onGetName();
+        Debug.Log("game object name length: " + characterCount);
+    }
+
+    //traditional implementation
+    /*int GetName()
+    {
+        return this.gameObject.name.Length;
+    }*/
+}
