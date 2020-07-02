ff3d35de7ad7318b29a267964187e13216e40af2
Jing <jtan1993@gmail.com>
2020-07-02 22:48:51 +0100

    added sum

---
+++ Assets/Scripts/DelegateEvents/DelegateReturnSumInt.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DelegateReturnSumInt : MonoBehaviour
+{
+    public Func<int, int, int> onCalculateSum; //input, input, output
+
+    private void Start()
+    {
+        onCalculateSum = (a, b) => a + b;
+        var total = onCalculateSum(5, 5);
+        Debug.Log(total);
+
+        /* more game logic
+        onCalculateSum = (a, b) =>
+        {
+            var total = a + b;
+            Debug.Log("sum: " + total);
+            return total; //must use return keyword
+        }; */
+
+    }
+
+    //traditional implementation
+    /*
+    int CalculateSum(int a, int b)
+    {
+        return a + b;
+    }*/
+}
