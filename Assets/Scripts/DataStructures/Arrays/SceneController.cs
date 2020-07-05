2e587514f6c60812d034326e603ed8148273d7d4
Jing <jtan1993@gmail.com>
2020-07-05 17:14:57 +0100

    added scenecontroller

---
+++ Assets/Scripts/DataStructures/Arrays/SceneController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    public string[] shapes = { "circle", "square", "triangle", "octagon"};
+
+    //tedious implementation without array
+    //public string nameOne = "circle";
+    //public string nameTwo = "square";
+    //public string nameThree = "triangle";
+    //public string nameFour = "octagon";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < shapes.Length; i++)
+        {
+            shapes[i] = shapes[i].ToUpper();
+            Debug.Log(shapes[i]);
+        }
+        //tedious implementation without array
+        //nameOne = nameOne.ToUpper();
+        //nameTwo = nameTwo.ToUpper();
+        //nameThree = nameThree.ToUpper();
+        //nameFour = nameFour.ToUpper();
+    }
+}
---
+++ Assets/Scripts/Shape.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shape : MonoBehaviour
+{
+    public string Name;
+
+    public void SetColor(Color newColor)
+    {
+        SpriteRenderer spriteRenderer =  GetComponent<SpriteRenderer>();
+        spriteRenderer.color = newColor;
+    }
+}
