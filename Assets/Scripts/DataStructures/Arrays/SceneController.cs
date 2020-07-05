b08c76070bf245a42e67f05242d839b5ffbe5858
Jing <jtan1993@gmail.com>
2020-07-05 18:10:08 +0100

    added obj

--- Assets/Scripts/DataStructures/Arrays/SceneController.cs
+++ Assets/Scripts/DataStructures/Arrays/SceneController.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 
 public class SceneController : MonoBehaviour
 {
-    public string[] shapes = { "circle", "square", "triangle", "octagon"}; //fixed in length
-    public string[] moreShapes = new string[4];
+    //array
+    public string[] shapesArray = { "circle", "square", "triangle", "octagon"}; //fixed in length
+    //public string[] moreShapes = new string[4];
 
-    //tedious implementation without array
+    //list
+    public List<string> shapesList;
+    public List<Shape> gameShapesList;
+
+    //tedious implementation without array / list
     //public string nameOne = "circle";
     //public string nameTwo = "square";
     //public string nameThree = "triangle";
@@ -16,12 +22,29 @@ public class SceneController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 0; i < shapes.Length; i++)
+        //array
+        for(int i = 0; i < shapesArray.Length; i++)
         {
-            shapes[i] = shapes[i].ToUpper();
-            Debug.Log(shapes[i]);
+            shapesArray[i] = shapesArray[i].ToUpper();
+            Debug.Log(shapesArray[i]);
         }
-        //tedious implementation without array
+
+        Shape octagon = gameShapesList.Find(s => s.Name == "Octagon"); //takes predicate: condition to match when searching list
+        octagon.SetColor(Color.red);
+
+        //list
+        //shapesList = new List<string>();
+        shapesList = new List<string> { "Hexagon", "Heptagon", "Rhombus", "Nonagon" };
+        shapesList.Add("Rectangle"); //can't do this with array
+        shapesList.Insert(2, "Diamond"); //add diamond at the 3rd position of the list
+        shapesList.Sort(); //output list alphabetically
+        for (int i = 0; i < shapesList.Count; i++)
+        {
+            shapesList[i] = shapesList[i].ToUpper();
+            Debug.Log(shapesList[i]);
+        }
+
+        //tedious implementation without array / list
         //nameOne = nameOne.ToUpper();
         //nameTwo = nameTwo.ToUpper();
         //nameThree = nameThree.ToUpper();
