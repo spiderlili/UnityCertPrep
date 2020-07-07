fd95fe6e45085b98c50bb41df6271d3ef0d4b7ce
Jing <jtan1993@gmail.com>
2020-07-07 22:40:03 +0100

    added playercontroller

---
+++ Assets/Scripts/Classes/GameSceneController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSceneController : MonoBehaviour
+{
+    public float playerSpeed;
+    public Vector3 screenBounds;
+
+    void Start()
+    {
+        playerSpeed = 10;
+        screenBounds = GetScreenBounds();
+    }
+
+    //returns the border of the screen in world space
+    private Vector3 GetScreenBounds()
+    {
+        Camera mainCamera = Camera.main;
+        Vector3 screenVector = new Vector3(Screen.width, Screen.height, mainCamera.transform.position.z);
+        return mainCamera.ScreenToWorldPoint(screenVector); 
+    }
+}
---
+++ Assets/Scripts/Classes/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Classes { 
+    public class PlayerController : Shape
+    {
+        private GameSceneController gameSceneController;
+       void Start()
+        {
+            //FindObjectOfType: generic function that finds and returns an instance of specified class
+            gameSceneController = FindObjectOfType<GameSceneController>(); 
+            SetColor(Color.yellow);
+        }
+
+    // Update is called once per frame
+        void Update()
+        {
+            MovePlayer();
+        }
+
+        private void MovePlayer()
+        {
+            float horizontalMovement = Input.GetAxis("Horizontal");
+
+            //test if L/R key is clicked, since value can be negative: use Abs and account for floating point precision
+            if (Mathf.Abs(horizontalMovement) > Mathf.Epsilon) 
+            {
+                //keep movement smooth between frames
+                horizontalMovement = horizontalMovement * Time.deltaTime * gameSceneController.playerSpeed;
+                horizontalMovement += transform.position.x;
+                transform.position = new Vector2(horizontalMovement, transform.position.y); //keep vertical pos the same
+            }
+        }
+    }
+}
