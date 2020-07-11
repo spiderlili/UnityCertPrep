4dadb15767b89ba1f9ed505f8cb11efc4b36056a
Jing <jtan1993@gmail.com>
2020-07-11 09:21:38 +0100

    coroutine

--- Assets/Scripts/Coroutines/CoroutineSceneController.cs
+++ Assets/Scripts/Coroutines/CoroutineSceneController.cs
@@ -6,11 +6,6 @@ public class CoroutineSceneController : MonoBehaviour
 {
     public List<Shape> gameShapes;
     public float secondsToWait = 1f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
@@ -18,6 +13,7 @@ public class CoroutineSceneController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             StartCoroutine(SetShapesBlue());
+            Time.timeScale = 0; //pause the game
             //SetShapesRed();
         }
     }
@@ -28,8 +24,15 @@ public class CoroutineSceneController : MonoBehaviour
         {
             shape.SetColor(Color.blue);
             //change colour 1 shape at a time with a slight delay between them
-            yield return new WaitForSeconds(secondsToWait);
+            //yield return new WaitForSeconds(secondsToWait); //execution of the coroutine is paused for x seconds, then resume
+
+            //works even if the game is paused with Time.timeScale = 0;
+            //useful for scenarios where you want to pause the game & have UI elements continue animating - can't do with Update()
+            yield return new WaitForSecondsRealtime(secondsToWait);
+            shape.SetColor(Color.white);
         }
+        yield return new WaitForSecondsRealtime(1); //can yield more than once in a coroutine
+        Debug.Log("just wasted a second");
     }
 
     private void SetShapesRed()
