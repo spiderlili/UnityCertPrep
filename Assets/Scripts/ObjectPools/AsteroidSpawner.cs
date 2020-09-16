a3f7ff434568036b84a6d94d5ed396338bc41e9d
Jing <jtan1993@gmail.com>
2020-09-16 08:51:59 +0100

    setup

---
+++ Assets/Scripts/ObjectPools/AsteroidSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour
+{
+    public GameObject asteroid;
+    public float randomXRange = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Random.Range(0, 100) < 3)
+        {
+            Instantiate(asteroid, this.transform.position + new Vector3(Random.Range(-randomXRange, randomXRange), 0, 0), Quaternion.identity);
+        }
+    }
+}
--- Assets/Scripts/ObjectPools/Drive.cs
+++ Assets/Scripts/ObjectPools/Drive.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class Drive : MonoBehaviour
 {
     public float speed = 10.0f;
+    public GameObject bullet;
 
     void Update()
     {
         float translation = Input.GetAxis("Horizontal") * speed;
         translation *= Time.deltaTime;
         transform.Translate(translation, 0, 0);
+
+        if (Input.GetKeyDown("space"))
+        {
+            Instantiate(bullet, this.transform.position, Quaternion.identity); //expensive to instantiate bullets all the time, taking up all memory
+        }
     }
 }
