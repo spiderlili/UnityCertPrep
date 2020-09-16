3f985c5cc881d55cee0a931a59bbd80e861f85e6
Jing <jtan1993@gmail.com>
2020-09-16 08:23:33 +0100

    created bullet pf

---
+++ Assets/Scripts/ObjectPools/Move.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public Vector3 velocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(velocity);
+    }
+}
