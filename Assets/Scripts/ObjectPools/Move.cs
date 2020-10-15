1ac65054ebfd8785c09bdcdd5043420c392cbfc5
Jing <jtan1993@gmail.com>
2020-10-15 09:57:46 +0100

    destroy asteroid

--- Assets/Scripts/ObjectPools/Move.cs
+++ Assets/Scripts/ObjectPools/Move.cs
@@ -6,6 +6,15 @@ public class Move : MonoBehaviour
 {
     public Vector3 velocity;
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //make sure it's not colliding with other things - only destroy the asteroid
+        if (collision.gameObject.tag == "Asteroid")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
