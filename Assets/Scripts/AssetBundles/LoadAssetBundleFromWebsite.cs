336dec7fe5660fa7d292c53085600f207e6e526c
Jing <jtan1993@gmail.com>
2020-10-19 21:37:51 +0100

    load asset from web

--- Assets/Scripts/AssetBundles/LoadAssetBundleFromWebsite.cs
+++ Assets/Scripts/AssetBundles/LoadAssetBundleFromWebsite.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class LoadAssetBundleFromWebsite : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private string assetBundleUrl = "https://github.com/spiderlili/UnityCertPrep/blob/master/Assets/BundledAssets/onlineBundle/playerbrutius";
+    private string prefabName = "Brutius.prefab";
+    //fallback url: http://files.holistic3d.com/Bundles/brutius
 
-    // Update is called once per frame
-    void Update()
+    // it must wait for the web call to occur
+    IEnumerator Start()
     {
-        
+        var unityWebRequestAB = UnityWebRequestAssetBundle.GetAssetBundle(assetBundleUrl);
+        //use yield as it can take sometime for the web request to occur depending on the internet connection - wait for that to happen
+        yield return unityWebRequestAB.SendWebRequest();
+
+        AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(unityWebRequestAB);
+        var loadAsset = bundle.LoadAssetAsync(prefabName);
+        //make asset load in sync with start so the program is not stuck during waiting if it's got other things to do
+        yield return loadAsset;
+
+        Instantiate(loadAsset.asset);
     }
 }
