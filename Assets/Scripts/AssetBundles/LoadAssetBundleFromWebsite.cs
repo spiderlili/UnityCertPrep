219f45922ba751264c8e9571d699eefd121f9d79
Jing <jtan1993@gmail.com>
2020-10-19 21:49:39 +0100

    load from online

--- Assets/Scripts/AssetBundles/LoadAssetBundleFromWebsite.cs
+++ Assets/Scripts/AssetBundles/LoadAssetBundleFromWebsite.cs
@@ -5,14 +5,14 @@ using UnityEngine.Networking;
 
 public class LoadAssetBundleFromWebsite : MonoBehaviour
 {
-    private string assetBundleUrl = "https://github.com/spiderlili/UnityCertPrep/blob/master/Assets/BundledAssets/onlineBundle/playerbrutius";
+    private string assetBundleUrl = "https://github.com/spiderlili/UnityCertPrep/blob/master/Assets/BundledAssets/onlineBundle/playerbrutius"; //Failed to decompress data
     private string prefabName = "Brutius.prefab";
-    //fallback url: http://files.holistic3d.com/Bundles/brutius
+    private string fallbackAssetBundleurl = "http://files.holistic3d.com/Bundles/brutius";
 
     // it must wait for the web call to occur
     IEnumerator Start()
     {
-        var unityWebRequestAB = UnityWebRequestAssetBundle.GetAssetBundle(assetBundleUrl);
+        var unityWebRequestAB = UnityWebRequestAssetBundle.GetAssetBundle(fallbackAssetBundleurl);
         //use yield as it can take sometime for the web request to occur depending on the internet connection - wait for that to happen
         yield return unityWebRequestAB.SendWebRequest();
 
