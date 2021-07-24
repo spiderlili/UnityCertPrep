e436689dfe71790ff0e46ea3cf94d8e4fa8a497a
Jing Tan <jtan1993@gmail.com>
2021-07-24 15:46:32 +0100

    exercise setup

---
+++ Assets/Courses/Colorado_OOP/Week3_EventHandlingMenus/Invoker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace ColoradoOOP
+{
+    // TODO: Copy one of the Timer scripts we've been using into the project. Declare fields for a timer and a no argument event object. 
+    public class Invoker : MonoBehaviour
+    {
+        private void Awake()
+        {
+            // TODO: Create an instance of the event object
+        }
+
+        public void AddNoArgumentListener()
+        {
+            // TODO: add a no-argument UnityAction delegate as a listener
+        }
+        
+        void Start()
+        {
+            // TODO: add a Timer component, set its duration to 1 second, run the timer.
+        }
+        
+        void Update()
+        {
+            // TODO: invoke the event, run the timer again after it's finished.
+        }
+    }  
+}
+
---
+++ Assets/Courses/Colorado_OOP/Week3_EventHandlingMenus/Listener.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ColoradoOOP
+{
+    public class Listener : MonoBehaviour
+    {
+        void Start()
+        {
+            // TODO: add code to get access to the Invoker class (by getting the Invoker component from the main camera & add your no argument method as a listener.
+        }
+    }
+}
+
