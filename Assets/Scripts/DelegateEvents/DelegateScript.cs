3ec30432f2617c41c1db199265dc5d5764e723e4
Jing Tan <jtan1993@gmail.com>
2021-07-25 16:18:40 +0100

    delegate script init

---
+++ Assets/Scripts/DelegateEvents/DelegateScript.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class DelegateScript : MonoBehaviour
+{
+    // Declare a delegate type: its instance references a method
+    internal delegate void MyDelegate(int num);
+    private MyDelegate myDelegate;
+    [SerializeField] private int number = 50;
+
+    private void Start()
+    {
+        // MyDelegate's instance (myDelegate) references the PrintNum method
+        myDelegate = PrintNum;
+        myDelegate(number);
+        
+        // MyDelegate's instance (myDelegate) references the DoubleNum method
+        myDelegate = DoubleNum;
+        myDelegate(number);
+    }
+
+    void PrintNum(int num)
+    {
+        Debug.Log("Print number: " + num);
+    }
+
+    void DoubleNum(int num)
+    {
+        Debug.Log("Double number: " + num * 2);
+    }
+
+}
