8326502c543a849165d991cc62a6984b7519a52c
Jing Tan <jtan1993@gmail.com>
2021-07-26 08:17:42 +0100

    Update DelegateScript.cs

--- Assets/Scripts/DelegateEvents/DelegateScript.cs
+++ Assets/Scripts/DelegateEvents/DelegateScript.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 public class DelegateScript : MonoBehaviour
 {
     // Declare a delegate type: its instance references a method
-    internal delegate void MyDelegate(int num);
+    internal delegate void MyDelegate(int num); // MyDelegate's return type = void, it can encapsulate a method which takes 1 int as an argument
     private MyDelegate myDelegate;
     [SerializeField] private int number = 50;
 
     private void Start()
     {
-        // MyDelegate's instance (myDelegate) references the PrintNum method
+        // MyDelegate's instance (myDelegate) calls the PrintNum method
         myDelegate = PrintNum;
         myDelegate(number);
         
-        // MyDelegate's instance (myDelegate) references the DoubleNum method
+        // MyDelegate's instance (myDelegate) calls the DoubleNum method
         myDelegate = DoubleNum;
         myDelegate(number);
     }
