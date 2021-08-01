48140054e35e6beb5001ec461597896ec93e7739
Jing Tan <jtan1993@gmail.com>
2021-08-01 14:13:09 +0100

    action

---
+++ Assets/Scripts/DelegateEvents/GameCombatDelegateExample/DelegateAnonymousMethodTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using TMPro;
+
+public class DelegateAnonymousMethodTest : MonoBehaviour
+{
+    [SerializeField] private TMP_Text introNameText;
+    [SerializeField] private TMP_Text introAgeText;
+    // Use anonymous method on Action<T> Delegate type
+    void Start()
+    {
+        Action<string> tellMeYourName = delegate(string name) {
+            string intro = "My name is ";
+            introNameText.text = intro + name;
+            Debug.Log(intro + name);
+        };
+        
+        Action<int> tellMeYourAge = delegate(int age) {
+            string intro = "My age is ";
+            string ageString = age.ToString();
+            introAgeText.text = intro + ageString;
+            Debug.Log(intro + ageString);
+        };
+
+        tellMeYourName("Kitty");
+        tellMeYourAge(26);
+    }
+}
