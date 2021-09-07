995c99e555e229a7c172f42d65e839095f457f80
Jing Tan <jtan1993@gmail.com>
2021-09-07 21:20:09 +0100

    sprite loading

--- Assets/Scripts/Addressables/AddressablesManager.cs
+++ Assets/Scripts/Addressables/AddressablesManager.cs
@@ -1,7 +1,9 @@
-﻿using UnityEngine.UI;
+﻿using System;
+using UnityEngine.UI;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.U2D; // For working with sprite atlases
 
 // TODO: configure sprite swapping, use the Addressables Event Viewer to see the memory beig managed for you, try other asset types (audioclips, materials, videoclips, etc)
 public class AddressablesManager : MonoBehaviour
@@ -11,6 +13,44 @@ public class AddressablesManager : MonoBehaviour
     [SerializeField] private AssetReferenceGameObject assetReferenceGameObject;
     [SerializeField] private AssetReference assetReferenceGameScene; // No specific type for a scene - general AssetReference
     [SerializeField] private AssetReferenceSprite assetReferenceSprite;
+    
+    [Tooltip("If game’s data is stored in a JSON or text file for projects that allow for user-edited content, or is subject to change at runtime: specify address for Sprite directly")]
+    [SerializeField] private string assetReferenceSpriteAddress;
+    [Tooltip("Enable useAddress to load octagon sprite, otherwise load triangle sprite by reference")]
+    [SerializeField] private bool useAddress;
+
+    [Tooltip("Parent Object")]
+    [SerializeField] private AssetReference mainObjectToLoad;
+    [Tooltip("Child Object")]
+    [SerializeField] private AssetReference accessoryObjectToLoad;
+    private GameObject instantiatedObject;
+    private GameObject instantiatedAccessoryObject;
+
+    private void Start()
+    {
+        Addressables.LoadAssetAsync<GameObject>(mainObjectToLoad).Completed += ObjectLoadDone;
+    }
+    
+    // Instantiate the object after it's loaded & instantiate the accessory child object
+    private void ObjectLoadDone(AsyncOperationHandle<GameObject> obj)
+    {
+        if (obj.Status == AsyncOperationStatus.Succeeded) {
+            GameObject loadedObject = obj.Result;
+            Debug.Log("Successfully loaded object!");
+            instantiatedObject = Instantiate(loadedObject);
+            Debug.Log("Successfully instantiated object!");
+
+            if (accessoryObjectToLoad != null) {
+                // InstantiateAsync loads & instantiates an Addressable Asset in a single step. LoadAssetAsync loads an Addressable Asset into memory, but does not instantiate it. 
+                accessoryObjectToLoad.InstantiateAsync(instantiatedObject.transform).Completed += op => {
+                    if (op.Status == AsyncOperationStatus.Succeeded) {
+                        instantiatedAccessoryObject = op.Result;
+                        Debug.Log("Successfully loaded & instantiated accessory child object!");
+                    }
+                };
+            }
+        }
+    }
     public void AddressablePrefab()
     {
         Addressables.InstantiateAsync(assetReferenceGameObject);
@@ -25,16 +65,32 @@ public class AddressablesManager : MonoBehaviour
     }
     
     // Callback: a method is called to load the asset - this happens on a separate thread
+    // registers a listener for the Completed callback of the load command, which is invoked immediately
     public void AddressableSprite()
     {
-        // When the LoadAssetAsync() completes: the Completed delegate is called
-        assetReferenceSprite.LoadAssetAsync().Completed += OnSpriteLoaded;
+        if (useAddress) {
+            // When the LoadAssetAsync() completes: the Completed delegate is called. LoadAssetAsync loads an Addressable Asset into memory, but does not instantiate it. 
+            Addressables.LoadAssetAsync<Sprite>(assetReferenceSpriteAddress).Completed += OnSpriteLoaded;
+        } else {
+            assetReferenceSprite.LoadAssetAsync().Completed += OnSpriteLoaded;
+        }
         Debug.Log("Load Addressable Sprite");
+        
     }
 
     // OnSpriteLoaded() has been passed to the Completed delegate as a callback
+    // code to be executed once Sprite is loaded can either be a delegate or, for simpler cases, a lambda expression.
     private void OnSpriteLoaded(AsyncOperationHandle<Sprite> handle)
     {
-        imageForAddressableSprite.sprite = handle.Result;
+        switch (handle.Status) {
+            case AsyncOperationStatus.Succeeded:
+                imageForAddressableSprite.sprite = handle.Result;
+                break;
+            case AsyncOperationStatus.Failed:
+                Debug.LogError("Sprite Load Failed!");
+                break;
+            default:
+                break;
+        }
     }
 }
