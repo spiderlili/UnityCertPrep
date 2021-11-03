7e0b1f2b59bf0e7b5bcebf88f607615bddc3f313
Jing Tan <jtan1993@gmail.com>
2021-11-03 15:23:20 +0000

    init

---
+++ Assets/Scripts/AsyncAwait/AsyncAwaitShape.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsyncAwaitShape : MonoBehaviour
+{
+    // Coroutine Example
+    public IEnumerator RotateForSeconds(float duration)
+    {
+        var end = Time.time + duration; // Time.time is the time in seconds at the beginning of this frame since the start of the application (Read Only).
+        while (Time.time < end) {
+            transform.Rotate(new Vector3(1, 1) * Time.deltaTime * 150);
+            yield return null; // Wait until the next frame & repeat until done
+        }
+    }
+}
---
+++ Assets/Scripts/AsyncAwait/AsyncAwaitShapeManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class AsyncAwaitShapeManager : MonoBehaviour
+{
+    [SerializeField] private AsyncAwaitShape[] shapes;
+    public void BeginTestRotate()
+    {
+        // Coroutine example: each loop will add a bit more time
+        for (var i = 0; i < shapes.Length; i++) {
+            StartCoroutine(shapes[i].RotateForSeconds(1 + 1 * i));
+        }
+    }
+
+}
