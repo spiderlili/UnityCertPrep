c8c26168ff8d9dc5ef57dacf7cc98c919662c5b8
Jing Tan <jtan1993@gmail.com>
2022-01-06 18:38:37 +0000

    add vfx player subscriber

---
+++ Assets/Scripts/DelegateEvents/ObserverPatternUnityEvents/LevelUpVFXSubscriber.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using System;
+
+public class LevelUpVFXSubscriber : MonoBehaviour
+{
+    [SerializeField] private LevelUpSubject levelUpSubject;
+    private void OnEnable()
+    {
+        if (levelUpSubject != null) {
+            levelUpSubject.ONLevelUpAction += PlayLevelUpVFX;
+        }
+        
+    }
+
+    private void OnDisable()
+    {
+        if (levelUpSubject != null) {
+            levelUpSubject.ONLevelUpAction -= PlayLevelUpVFX;
+        }
+    }
+
+    private void PlayLevelUpVFX()
+    {
+        this.GetComponent<ParticleSystem>().Play();
+    }
+}
