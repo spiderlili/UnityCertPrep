f6147c6e50ba5703f050f92f5b9c0ceaf098f4cf
Jing Tan <jtan1993@gmail.com>
2022-04-05 15:28:34 +0100

    add count down functionality

--- Assets/CatlikeCoding/CountUpTimer.cs
+++ Assets/CatlikeCoding/CountUpTimer.cs
@@ -2,21 +2,66 @@
 using UnityEngine;
 using TMPro;
 
+// TODO: Add flashing effect once time is up
 public class CountUpTimer : MonoBehaviour{
-    public TMP_Text m_ClockText;
+    // TODO: Delete
+    [SerializeField] private TMP_Text m_ClockText;
     private float m_Timer;
     private int m_Hour = 0;
     private int m_Minute = 0;
     private int m_Second = 0;
     
-    void Start ()
+    // Set timer duration
+    [SerializeField] private float timeDurationMinutes = 3f;
+    private float timeDurationSeconds;
+    private float timer;
+     
+    // Separate timer objects
+    [SerializeField] private TextMeshProUGUI firstMinute; // First slot for minutes
+    [SerializeField] private TextMeshProUGUI secondMinute; // Second slot for minutes
+    [SerializeField] private TextMeshProUGUI firstSecond; // First slot for seconds
+    [SerializeField] private TextMeshProUGUI secondSecond; // Second slot for seconds
+    [SerializeField] private TextMeshProUGUI timerSeparator; // First slot for seconds
+
+    // Set current time to the time duration, subtract the time that has elapsed from the timer
+    private void ResetTimer()
     {
+        timer = timeDurationSeconds;
+        
+        // TODO: Delete
         m_Hour = 0;
         m_Minute = 0;
         m_Second = 0;
     }
     
-    void Update () {
+    private void Flash()
+    {
+        
+    }
+
+    private void UpdateTimerDisplay(float timeInSeconds)
+    {
+        float minutes = Mathf.FloorToInt(timeInSeconds / 60); // Round value down so when you're at 0 you get 0
+        float seconds = Mathf.FloorToInt(timeInSeconds % 60);
+        string currentTime = string.Format("{00:00}{1:00}", minutes, seconds);
+        firstMinute.text = currentTime[0].ToString();
+        secondMinute.text = currentTime[1].ToString();
+        firstSecond.text = currentTime[2].ToString();
+        secondSecond.text = currentTime[3].ToString();
+
+    }
+    private void UpdateTimerWithFlashOnEnd()
+    {
+        if (timer > 0) {
+            timer -= Time.deltaTime;
+            UpdateTimerDisplay(timer);
+        } else {
+            Flash();
+        }
+    }
+    
+    private void SetTimerTextOneString()
+    {
         m_Timer += Time.deltaTime;
         m_Second = (int)m_Timer;     
         if (m_Second > 59.0f)
@@ -34,5 +79,23 @@ public class CountUpTimer : MonoBehaviour{
             m_Timer = 0;
         }
         m_ClockText.text = string.Format("{0:d2}:{1:d2}:{2:d2}", m_Hour,m_Minute,m_Second); 
+    }
+    
+    private void Awake()
+    {
+        timeDurationSeconds = timeDurationMinutes * 60f;
+    }
+    
+    void Start ()
+    {
+        ResetTimer();
+    }
+
+    void Update ()
+    {
+        UpdateTimerWithFlashOnEnd();
+        
+        // TODO: Delete
+        SetTimerTextOneString();
     }
 }
