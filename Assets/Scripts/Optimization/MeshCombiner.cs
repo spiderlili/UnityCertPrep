1769b94b4c5381d5d24e987882848ed8e26e141c
Jing Tan <jtan1993@gmail.com>
2022-04-26 09:21:57 +0100

    add meshcombiner

--- Assets/Scripts/Optimization/MeshCombiner.cs
+++ Assets/Scripts/Optimization/MeshCombiner.cs
@@ -1,12 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+// TODO: Make editor window to save mesh: run CombineAllMeshes & save to disk
+// TODO: Make material
 public class MeshCombiner : MonoBehaviour
 {
-    void MeshCombine()
+    private void Start()
+    {
+        CombineAllMeshes();
+    }
+    
+    public void CombineAllMeshes()
     {
         MeshFilter[] filters = GetComponentsInChildren<MeshFilter>();
-        
+        CombineInstance[] combineInstances = new CombineInstance[filters.Length];
+
+        for (int i = 0; i < filters.Length; i++) {
+            combineInstances[i].mesh = filters[i].sharedMesh;
+            combineInstances[i].transform = filters[i].transform.localToWorldMatrix;
+        }
+
+        Mesh finalMesh = new Mesh();
+        finalMesh.CombineMeshes(combineInstances);
+
+        GetComponent<MeshFilter>().sharedMesh = finalMesh;
     }
 }
