c76f9026affc1c292190dc076744af85e013a53a
Jing Tan <jtan1993@gmail.com>
2022-04-26 09:26:16 +0100

    tweaked level

--- Assets/Scripts/Optimization/MeshCombiner.cs
+++ Assets/Scripts/Optimization/MeshCombiner.cs
@@ -4,6 +4,7 @@
 // TODO: Make material
 public class MeshCombiner : MonoBehaviour
 {
+    [SerializeField]private GameObject meshGroup;
     private void Start()
     {
         CombineAllMeshes();
@@ -11,17 +12,19 @@ public class MeshCombiner : MonoBehaviour
     
     public void CombineAllMeshes()
     {
-        MeshFilter[] filters = GetComponentsInChildren<MeshFilter>();
-        CombineInstance[] combineInstances = new CombineInstance[filters.Length];
+        if (meshGroup != null) {
+            MeshFilter[] filters = meshGroup.GetComponentsInChildren<MeshFilter>();
+            CombineInstance[] combineInstances = new CombineInstance[filters.Length];
 
-        for (int i = 0; i < filters.Length; i++) {
-            combineInstances[i].mesh = filters[i].sharedMesh;
-            combineInstances[i].transform = filters[i].transform.localToWorldMatrix;
+            for (int i = 0; i < filters.Length; i++) {
+                combineInstances[i].mesh = filters[i].sharedMesh;
+                combineInstances[i].transform = filters[i].transform.localToWorldMatrix;
+            }
+
+            Mesh finalMesh = new Mesh();
+            finalMesh.CombineMeshes(combineInstances);
+
+            GetComponent<MeshFilter>().sharedMesh = finalMesh;
         }
-
-        Mesh finalMesh = new Mesh();
-        finalMesh.CombineMeshes(combineInstances);
-
-        GetComponent<MeshFilter>().sharedMesh = finalMesh;
     }
 }
