c4b8e0fa40cb8fcf68726c738ff9bb7876637d07
Jing Tan <jtan1993@gmail.com>
2022-09-19 16:58:40 +0100

    Update CoroutineScreenshotPNG.cs

--- Assets/Scripts/Coroutines/CoroutineScreenshotPNG.cs
+++ Assets/Scripts/Coroutines/CoroutineScreenshotPNG.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class CoroutineScreenshotPNG : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(ScreenshotPNG());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator ScreenshotPNG()
     {
-        
+        yield return new WaitForEndOfFrame();
+        int width = Screen.width;
+        int height = Screen.height;
+        Texture2D tex = new Texture2D(width, height, TextureFormat.RGB24, false);
+        tex.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+        tex.Apply();
+        byte[] bytes = tex.EncodeToPNG();
+        Destroy(tex);
+        File.WriteAllBytes(Application.dataPath + "/../Screenshot.png", bytes);
     }
 }
