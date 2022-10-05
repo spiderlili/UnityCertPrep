1bf2423fc76482ee1014017424d0eef936e2fe77
Jing Tan <jtan1993@gmail.com>
2022-10-05 09:37:23 +0100

    Update UseLambdaAsDelegateExample.cs

--- Assets/Scripts/DelegateEvents/LambdaDelegates/UseLambdaAsDelegateExample.cs
+++ Assets/Scripts/DelegateEvents/LambdaDelegates/UseLambdaAsDelegateExample.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class UseLambdaAsDelegateExample : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public delegate void myEventHandler(string value); // Define a delegate
+    private string theVal;
+    public event myEventHandler valueChanged;
+    public string val
+    {
+        set {
+            this.theVal = value;
+            this.valueChanged(theVal); // When the value changes: fire the event
+        }
+    }
+    
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Use a lambda expression to define an event handler. Note this is a statement lambda due to the use of {}
+        valueChanged += (x) => {
+            Debug.Log("The value changed to " + x);
+        };
     }
 }
